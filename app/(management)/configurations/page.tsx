cc3a65f7527427e97e98c08d6293b1399c1247a8
ThanhDong00 <thanhdonganmy@gmail.com>
2025-01-02 16:45:01 +0700

    feat: config (part2)

--- app/(management)/configurations/page.tsx
+++ app/(management)/configurations/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import React from "react";
-import Layout from "@/components/Layout";
+import { useQuery } from "@tanstack/react-query";
+
+import { OrgConfigService } from "@/api/OrgConfigService";
 
 const Configurations = () => {
-  return (
-    <div>Configurations</div>
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["org-config"],
+    queryFn: async () => {
+      const response = await OrgConfigService.getConfig();
+      return response;
+    },
+  });
 
-  );
+  console.log("data", data);
+
+  return <div>Configurations</div>;
 };
 
 export default Configurations;
